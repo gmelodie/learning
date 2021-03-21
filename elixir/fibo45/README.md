78c112346bb81d7656f740271cf49b4a398688eb
Gabriel Cruz <gabrieldemelocruz@gmail.com>
2021-03-21 11:40:02 -0300

    Add fibo45

---
+++ elixir/fibo45/README.md
@@ -0,0 +1,2 @@
+# Fibo45
+Finds the 45th number in the fibonacci function
---
+++ elixir/fibo45/fibo45.ex
@@ -0,0 +1,32 @@
+require Time
+
+defmodule Fibo45 do
+  @correct_45th_number 1134903170
+
+  def fibo(stop_idx, curr_idx \\ 3, last_num \\ 1, second_to_last_num \\ 1)
+
+  def fibo(stop_idx, curr_idx, last_num, _) when curr_idx == stop_idx + 1 do last_num end
+  def fibo(stop_idx, _, _, _) when stop_idx < 3 do IO.puts("Invalid stop index. Needs to be higher than 2") end
+
+  def fibo(stop_idx, curr_idx, last_num, second_to_last_num) do
+    curr_num = last_num + second_to_last_num
+    fibo(stop_idx, curr_idx+1, curr_num, last_num)
+  end
+
+  def find do
+    fibo(45)
+  end
+
+  def time do
+    start = Time.utc_now()
+
+    ans = find()
+    if ans != @correct_45th_number do
+      IO.puts("Incorrect number! Expected #{} got #{}")
+    end
+
+    stop = Time.utc_now()
+    runtime = Time.diff(stop, start, :microsecond)
+    IO.puts("Number calculated in #{runtime} microseconds (1e-6s)")
+  end
+end

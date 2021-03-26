26bec2c3a5abfaec6236cfa52e2c9f08c37cff56
Gabriel Cruz <gabrieldemelocruz@gmail.com>
2021-03-26 15:34:19 -0300

    Add count_chars and count_lines

--- elixir/word_count/lib/word_count.ex
+++ elixir/word_count/lib/word_count.ex
@@ -5,14 +5,32 @@ defmodule WordCount do
 
     [filename | _] = args
     content = File.read!(filename)
+    if parsed[:chars] do
+      count_chars(content)
+    end
     if parsed[:words] do
       count_words(content)
     end
+    if parsed[:lines] do
+      count_lines(content)
+    end
+  end
+
+  def count_chars(content) do
+    content
+    |> String.replace(~r{\n}, "", trim: true)
+    |> String.length
   end
 
   def count_words(content) do
     content
-      |> String.split(~r{[\s\,\.]+}, trim: true)
-      |> Enum.count()
+    |> String.split(~r{[\s\,\.]+}, trim: true)
+    |> Enum.count()
+  end
+
+  def count_lines(content) do
+    content
+    |> String.split(~r{\n|\r|\r\n}, trim: true)
+    |> Enum.count()
   end
 end
--- elixir/word_count/test/word_count_test.exs
+++ elixir/word_count/test/word_count_test.exs
@@ -2,11 +2,29 @@ defmodule WordCountTest do
   use ExUnit.Case
   doctest WordCount
 
-  test "count words" do
+  test "counts chars" do
+    input = """
+    Hello, this file contains 34 chars
+    """
+    expected = 34
+    assert WordCount.count_chars(input) == expected
+  end
+
+  test "counts words" do
     input = """
     Hello, this file contains 6 words
     """
     expected = 6
     assert WordCount.count_words(input) == expected
+  end
+
+  test "counts lines" do
+    input = """
+    Hello, this file contains
+    3
+    lines\n
+    """
+    expected = 3
+    assert WordCount.count_lines(input) == expected
   end
 end
